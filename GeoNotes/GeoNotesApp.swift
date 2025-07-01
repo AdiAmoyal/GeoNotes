d483c705d2085d9e3a62fb65cc506b9057b98b50
AdiAmoyal <adi.amoyal02@gmail.com>
2025-07-01 14:14:56 +0300

    Initial Commit

---
+++ GeoNotes/GeoNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoNotesApp.swift
+//  GeoNotes
+//
+//  Created by Adi Amoyal on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GeoNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
