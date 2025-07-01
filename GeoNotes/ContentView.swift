6c69bf0f1bc997f2069b6129937a86200b2e9b33
AdiAmoyal <adi.amoyal02@gmail.com>
2025-07-01 14:58:26 +0300

    Add App icon, launch screen and colors pallete

--- GeoNotes/ContentView.swift
+++ GeoNotes/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        VStack(spacing: 20) {
+            
             Text("Hello, world!")
+                .foregroundStyle(Color.accentColor)
+            
+            Text("Hello, world!")
+                .foregroundStyle(Color.secondary)
+            
+            Text("Hello, world!")
+                .foregroundStyle(Color.background)
+            
+            Text("Hello, world!")
+                .foregroundStyle(Color.text)
         }
         .padding()
     }
